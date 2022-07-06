5256b928d0c11fa064e3b0dff1f2344f65e87229
Zack Johnston <zackjohnston@me.com>
2022-07-06 17:39:55 -0500

    calculator structure completed

---
+++ script.js
@@ -0,0 +1,6 @@
+const display = document.querySelector('.display');
+const clears = document.querySelector('.clears');
+const equals = document.querySelector('.equals');
+const numbers = document.querySelectorAll('.numbers');
+const operators = document.querySelectorAll('.operators');
+
